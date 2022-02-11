d1352712ea720de28b6e82bea52446884b77644e
Zach Prince <zachary.s.prince@outlook.com>
2022-02-10 22:00:49 -0500

    Complete methods.rb

--- 2section/exercises/methods.rb
+++ 2section/exercises/methods.rb
@@ -5,28 +5,43 @@
 
 # Example: Write a method that when called will print your name:
 def print_name
-  p "Severus Snape"
+  p "Zachary Prince"
 end
 
 print_name
 
 # Write a method that takes a name as an argument and prints it:
 def print_name(name)
-  # YOUR CODE HERE
+  puts name
 end
 
 print_name("Albus Dumbledore")
 
 # Write a method that takes in 2 numbers as arguments and prints 
 # their sum. Then call your method three times with different arguments passed in:
-# YOUR CODE HERE
+def print_num(num1, num2)
+  puts num1, num2
+end
 
+print_num(8, 9)
+
+print_num(4, 7)
+
+print_num(1, 9)
 
 # Write a method that takes in two strings as arguments and prints 
 # a concatenation of those two strings. Example: The arguments could be 
 # (man, woman) and the end result might output: "When Harry Met Sally".  
 # Then call your method three times with different arguments passed in. 
+def pets(pet1, pet2)
+  puts "I have a #{pet1} and a #{pet2}!"
+end
 
+pets("lizard", "cat")
+
+pets("tiger", "alliagtor")
+
+pets("salamander", "coyote")
 
 #-------------------
 # PART 3: Naming is Hard
@@ -39,8 +54,9 @@ print_name("Albus Dumbledore")
 
 # Look at the code you wrote for the previous YOU DO🎈 - what did you name the
 # function, and why?
-# YOUR ANSWER HERE
+
+# I named it "pets" to indicate that the method owuld store pets.
 
 # What did you name each parameter, and why?
-# YOUR ANSWER HERE
+# I named the parameters "pet1" and "pet2" to reflect their method and their place in the function.
 
